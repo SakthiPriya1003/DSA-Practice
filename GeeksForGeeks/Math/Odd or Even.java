a5cb31c097b9c188d6dddd94ee617e0f24d96728
SakthiPriya1003 <sakthipriyagit@gmail.com>
2025-12-14 22:35:53 +0530

    Add Odd_or_Even (GFG)

---
+++ GeeksForGeeks/Math/Odd or Even.java
@@ -0,0 +1,21 @@
+class Solution {
+    static boolean isEven(int n) {
+        if(n % 2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+/*Approach
+
+Check whether the given number is even by using the modulo operator.
+If n % 2 equals 0, the number is even.
+
+Time Complexity:
+O(1)
+
+Space Complexity:
+O(1)
+*/
